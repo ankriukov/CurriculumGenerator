1c7b36cda6c339c4592d7e4c458e42eea775b0df
Антон Крюков <toxakrukov@ukr.net>
2017-10-05 19:23:43 +0300

    filled

--- Curriculum/Model/Commands.cs
+++ Curriculum/Model/Commands.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Curriculum.ViewModel
 {
-    public class Command
+    public class Commands
     {
+        //private ICommand 
     }
 }
