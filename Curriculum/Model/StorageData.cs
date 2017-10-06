bb11238203b2f84c51e3143f7518658e511548e1
Антон Крюков <toxakrukov@ukr.net>
2017-10-06 12:12:19 +0300

    частично отредактирована

---
+++ Curriculum/Model/StorageData.cs
@@ -0,0 +1,31 @@
+﻿using DataDll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curriculum.Model
+{
+    public static class StorageData
+    {
+        #region Lists of objects
+        public static List<WorkDay> WorkDays { get; set; }
+        public static List<Room> Rooms { get; set; }
+        public static List<Group> Groups { get; set; }
+        public static List<Lesson> Lessons { get; set; }
+        public static List<Pair> Pairs { get; set; }
+        public static List<Teacher> Teachers { get; set; }
+        #endregion
+
+        static StorageData()
+        {
+            WorkDays = new List<WorkDay>();
+            Rooms = new List<Room>();
+            Teachers = new List<Teacher>();
+            Pairs = new List<Pair>();
+
+        }
+
+    }
+}
---
+++ Curriculum/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DataDll;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curriculum.ViewModel
+{
+    public class MainViewModel 
+    {
+        DBContext db = new DBContext();
+
+        public MainViewModel()
+        {
+            var a = db.Groups;
+        }
+    }
+}
