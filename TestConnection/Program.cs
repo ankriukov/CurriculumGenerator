7f90d511e826f3f56e4f67ab0d7de62a0a8bdd26
Антон Крюков <toxakrukov@ukr.net>
2017-10-09 12:38:56 +0300

    рабочий этап. Готовые 1-ая и 3-ая вкладки

---
+++ TestConnection/Program.cs
@@ -0,0 +1,28 @@
+﻿using DataDll;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string databaseName = @"C:\Users\Sony\Desktop\CurriculumGenerator\DataDll\DBCurriculum.db";
+            SQLiteConnection connection =
+            new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+            connection.Open();
+            SQLiteCommand command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;", connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            foreach (DbDataRecord record in reader)
+                Console.WriteLine("Таблица: " + record["name"]);
+            connection.Close();
+        }
+    }
+}
