e4a96ce5d9403a35a58c6cfb5ddde223c0850c80
Anton <an.kr.working@gmail.com>
2017-10-10 19:47:58 +0300

    asx

--- Curriculum/Model/StorageData.cs
+++ Curriculum/Model/StorageData.cs
@@ -16,15 +16,38 @@ namespace Curriculum.Model
         public static List<Lesson> Lessons { get; set; }
         public static List<Pair> Pairs { get; set; }
         public static List<Teacher> Teachers { get; set; }
+        public static List<TypeLesson> TypeLessons { get; set; }
+        public static List<LessonTypeLesson> LessonTypeL { get; set; }
         #endregion
 
         static StorageData()
         {
-            WorkDays = new List<WorkDay>();
-            Rooms = new List<Room>();
-            Teachers = new List<Teacher>();
-            Pairs = new List<Pair>();
+            WorkDays = DBMethods.getWorkDay();
+            Rooms = DBMethods.getRoom();
+            Teachers = DBMethods.getTeacher();
+            Pairs = DBMethods.getPair();
+            Lessons = DBMethods.getLesson();
+            TypeLessons = DBMethods.getTypeLesson();
+            LessonTypeL = DBMethods.getLesTypeLes();
+        }
 
+        public static List<dynamic> GetTypesByLesson(string lesson)
+        {
+            var id_name = Lessons.Where(x => x.Name == lesson).Select(x => x.Id).FirstOrDefault();
+            var ltl = (from lesTypeles in LessonTypeL
+                      where lesTypeles.Id_Lesson == id_name 
+
+            //var tr = from it in ltl
+            //         join tpLess in TypeLessons
+            //         on it.Id_TypeLesson equals tpLess.Id
+            //         select new
+            //         {
+            //             NameLesson = lesson,
+                         
+            //         }
+
+
+            return null;
         }
 
     }
